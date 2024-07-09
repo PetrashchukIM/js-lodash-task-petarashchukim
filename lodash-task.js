fad7b950eedc4b80cab732966a7df7324ebac56f
PetrashchukIM <petrashchukim@gmail.com>
2024-07-10 00:12:25 +0300

    create  lodash-task.js

---
+++ lodash-task.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+
+// Returns an array of active users.
+function getActiveUsers(users) {
+
+}
+
+
+//Returns an array of users sorted by age in ascending order.
+function getSortedUsersByAge(users) {
+
+}
+
+
+//Returns an array of all users' names.
+function getUserNames(users) {
+
+}
+
+//Returns the user object by the given id. If the user is not found, returns null.
+function getUserById(users, id) {
+
+}
+
+//Adds a new user to the array of users. Returns the updated array of users.
+function addUser(users, newUser) {
+
+}
+
+//Removes the user by the given id. Returns the updated array of users.
+function removeUserById(users, id) {
+    
+}
